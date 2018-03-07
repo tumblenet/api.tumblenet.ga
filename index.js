d1027f024babd2433d85870c87297bf94c06e9e0
Tumble <tumblegamer@gmail.com>
2018-03-07 22:36:27 +0000

    hello world

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const http = require('http');
 
 var app = function (req, res) {
-  res.end("hello")
+  res.end("Hello World")
 }
 var port = 3000;
 var server = new http.Server(app);
-
-console.log("Hello World");
-
 
 server.listen(port, function () {
   console.log("Server listening on port " + port);
