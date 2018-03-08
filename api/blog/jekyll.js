5c71ecb45d1c2a3d720b349440b1530ca699bb7f
Tumble <tumblegamer@gmail.com>
2018-03-08 20:34:11 +0000

    tags

--- api/blog/jekyll.js
+++ api/blog/jekyll.js
@@ -3,28 +3,31 @@ const getJSON = require('./get-json.js');
 function getJekyllJson(url) {
   var feedUrl = url + "/feed/";
 
+
   var feed = {
     categories:[],
     tags: [],
     posts: []
-  }
+  };
 
 
   //categories
   getJSON(feedUrl + "categories.json", function (json) {
+    //console.log(json);
     var categories = Object.keys(json);
     feed.categories = categories;
-  });
 
-  //tags
-  getJSON(feedUrl + "tags.json", function (json) {
-  });
+    //tags
+    getJSON(feedUrl + "tags.json", function (json) {
+      var tags = Object.keys(json);
+      feed.tags = tags;
+      //posts
+      getJSON(feedUrl + "posts.json", function (json) {
 
-  //posts
-  getJSON(feedUrl + "posts.json", function (json) {
+        console.log(feed);
+      });
+    });
   });
-
-  console.log(feed);
 }
 
 module.exports = getJekyllJson;
