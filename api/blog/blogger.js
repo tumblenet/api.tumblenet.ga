f2e4290aae55d81101fe0b6fe8cc74817faf2c08
Tumble <tumblegamer@gmail.com>
2018-03-08 19:30:42 +0000

    testing categories

--- api/blog/blogger.js
+++ api/blog/blogger.js
@@ -3,8 +3,18 @@ const getJSON = require('./get-json.js');
 function getBloggerJson(url) {
   var feedUrl = url + "/feeds/posts/default?alt=json";
 
-  getJSON(feedUrl, function (feed) {
-    console.log(feed);
+  getJSON(feedUrl, function (json) {
+    var feed = {
+      categories = [],
+      tags = [],
+      posts = [];
+    }
+
+    json.feed.category.forEach(function (catObj) {
+      console.log(catObj);
+    });
+
+    //console.log(json);
   });
 }
 
