dc1d360b183f12f0d6180bcf91294c70993884ab
Tumble <tumblegamer@gmail.com>
2018-03-08 19:22:35 +0000

    blogger

--- api/blog/blogger.js
+++ api/blog/blogger.js
@@ -3,7 +3,7 @@ const getJSON = require('./get-json.js');
 function getBloggerJson(url) {
   var feedUrl = url + "/feeds/posts/default?alt=json";
 
-  getJSON(url, function (feed) {
+  getJSON(feedUrl, function (feed) {
     console.log(feed);
   });
 }
--- server.js
+++ server.js
@@ -8,4 +8,4 @@ server.listen(port, function () {
   console.log("Server listening on port " + port);
 });
 
-require('./api/blog/blogger.js')("tumblegamer.blog.tumblenet.ga");
+require('./api/blog/blogger.js')("http://tumblegamer.blog.tumblenet.ga");
