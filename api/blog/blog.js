f820035c94deaf52fb8cf07b574215bd965cb52e
Tumble <tumblegamer@gmail.com>
2018-03-08 20:58:36 +0000

    getBlogs

--- api/blog/blog.js
+++ api/blog/blog.js
@@ -2,10 +2,18 @@ const getBlogger = require('./json/blogger.js');
 const getJekyll = require('./json/jekyll.js');
 
 function getBlogs() {
-  var feed = {
-    categories: [],
-    tags: [],
-    posts: []
-  }
-  
+
+  getJekyll("http://tumblenet.ga", function (feed) {
+    var tnBlog = feed;
+    getBlogger("http://tumblegamer.blog.tumblenet.ga", function (feed) {
+      var tgBlog = feed;
+      getBlogger("http://doctorbatmanwho.blog.tumblenet.ga", function (feed) {
+        var dbwBlog = feed;
+        var test = Object.assign(tbBlog,tgBlog,dbwBlog);
+        console.log(test);
+      });
+    });
+  });
 }
+
+module.exports = getBlogs;
