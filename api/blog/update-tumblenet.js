e584002c9823e249145317b681a52b975ed5580d
Tumble <tumblegamer@gmail.com>
2018-03-09 17:20:17 +0000

    = to :

--- api/blog/update-tumblenet.js
+++ api/blog/update-tumblenet.js
@@ -2,14 +2,14 @@ const octokit = require('@octokit/rest')();
 const getBlog = require('./blog.js');
 
 var result = octokit.repos.updateFile({
-  owner = "tumblenet",
-  repo = "beta.tumblenet.cu.cc",
-  path = "_data/blog.json",
+  owner: "tumblenet",
+  repo: "beta.tumblenet.cu.cc",
+  path: "_data/blog.json",
   message,
-  content = getBlog(),
+  content: getBlog(),
   sha,
-  branch = master,
-  committer = "TumbleNet API",
+  branch: master,
+  committer: "TumbleNet API",
   author
 });
 console.log(result);
