b137e8e7f0e5703e7c6e71cb87fd9b004721ceda
Tumble <tumblegamer@gmail.com>
2018-03-09 00:16:23 +0000

    category filter

--- api/blog/router.js
+++ api/blog/router.js
@@ -9,6 +9,25 @@ tags
 posts/yyyy/mm/dd
 */
 
+router.use("/categories/:category",function (req, res) {
+  getBlog(function(blog) {
+    res.json(blog.posts.filter(function (post) {
+      console.log(post.category);
+      if (typeof post.category == "string") {
+        return post.category === req.param.category;
+      } else {
+        return post.category.includes(req.param.category);
+      }
+    }));
+  });
+});
+
+router.use("/tags/:tag",function (req, res) {
+  getBlog(function(blog) {
+    res.json(blog.posts.filter(post => post.tags.includes(req.param.tags)));
+  });
+});
+
 router.use("/categories",function (req, res) {
   getBlog(function(blog) {
     res.json(blog.categories);
