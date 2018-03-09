3cf4061773bd6053e2d24abdb06b32d64f3b0f74
Tumble <tumblegamer@gmail.com>
2018-03-09 15:14:51 +0000

    get

--- api/blog/router.js
+++ api/blog/router.js
@@ -9,28 +9,55 @@ tags
 posts/yyyy/mm/dd
 */
 
-
-router.use("/tags/:tag",function (req, res) {
+router.get("/tags/:tag",function (req, res) {
   getBlog(function(blog) {
     res.json(blog.posts.filter(post => post.tags.includes(req.params.tag)));
   });
 });
 
-router.use("/tags",function (req, res) {
+router.get("/tags",function (req, res) {
   getBlog(function(blog) {
     res.json(blog.tags);
   });
 });
 
+router.get("/categories/:category",function (req, res) {
+  getBlog(function(blog) {
+    res.json(blog.posts.filter(function (post) {
+      if (typeof post.category == "string") {
+        return post.category === req.params.category;
+      } else {
+        return post.category.includes(req.params.category)
+      }
+    }));
+  });
+});
 
-router.use("/categories",function (req, res) {
+router.get("/categories",function (req, res) {
   getBlog(function(blog) {
     res.json(blog.categories);
   });
 });
 
+router.get("/:year/:month?/:day?", function (req, res) {
+  var query = req.params;
+  res.send(query);
+  getBlog(function (blog) {
+    //res.json(
+      blog.posts.filter(function (post) {
+      var testDate = post.date;
+      testDate.setDate(query.day);
+      testDate.setMonth(query.month);
+      testDate.setFullYear(query.year);
+      console.log(testDate.toJSON());
+      return true;
+    })
+  //);
+  });
+});
 
-router.use("/",function (req, res) {
+
+router.get("/",function (req, res) {
   getBlog(function(blog) {
     res.json(blog.posts);
   });
