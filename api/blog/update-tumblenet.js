d4972b767d57fa3595302d0a5792d6fa09eccaea
Tumble <tumblegamer@gmail.com>
2018-03-09 20:01:45 +0000

    commit :smiley:

--- api/blog/update-tumblenet.js
+++ api/blog/update-tumblenet.js
@@ -1,84 +1,20 @@
-const rimraf = require('rimraf');
-const Git = require("nodegit");
-const path = require("path");
-const promisify = require("promisify-node");
-const fse = promisify(require("fs-extra"));
+const Github = require('github-api');
+const path = require('path');
 
-var fileName = "newfile.txt";
-var fileContent = "hello world";
-var directoryName = "salad/toast/strangerinastrangeland/theresnowaythisexists";
-// ensureDir is an alias to mkdirp, which has the callback with a weird name
-// and in the 3rd position of 4 (the 4th being used for recursion). We have to
-// force promisify it, because promisify-node won't detect it on its
-// own and assumes sync
-fse.ensureDir = promisify(fse.ensureDir);
+var github = new Github({
+  token: require('../../config/oauth.js').token || process.env.OAUTH_TOKEN,
+  auth: "oauth"
+});
 
-/**
- * This example creates a certain file `newfile.txt`, adds it to the git
- * index and commits it to head. Similar to a `git add newfile.txt`
- * followed by a `git commit`
-**/
 
-var repo;
-var index;
-var oid;
-Git.Clone("https://github.com/tumblenet/beta.tumblenet.cu.cc", path.resolve(__dirname, "tmp"))
-.then(function () {
-  Git.Repository.open(path.resolve(__dirname, "tmp/.git"))
-  .then(function(repoResult) {
-    repo = repoResult;
-    return fse.ensureDir(path.join(repo.workdir(), directoryName));
-  }).then(function(){
-    return fse.writeFile(path.join(repo.workdir(), fileName), fileContent);
-  })
-  .then(function() {
-    return fse.writeFile(
-      path.join(repo.workdir(), directoryName, fileName),
-      fileContent
-    );
-  })
-  .then(function() {
-    return repo.refreshIndex();
-  })
-  .then(function(indexResult) {
-    index = indexResult;
-  })
-  .then(function() {
-    // this file is in the root of the directory and doesn't need a full path
-    return index.addByPath(fileName);
-  })
-  .then(function() {
-    // this file is in a subdirectory and can use a relative path
-    return index.addByPath(path.posix.join(directoryName, fileName));
-  })
-  .then(function() {
-    // this will write both files to the index
-    return index.write();
-  })
-  .then(function() {
-    return index.writeTree();
-  })
-  .then(function(oidResult) {
-    oid = oidResult;
-    return Git.Reference.nameToId(repo, "HEAD");
-  })
-  .then(function(head) {
-    return repo.getCommit(head);
-  })
-  .then(function(parent) {
-    var author = Git.Signature.create("Scott Chacon",
-    "schacon@gmail.com", 123456789, 60);
-    var committer = Git.Signature.create("Scott A Chacon",
-    "scott@github.com", 987654321, 90);
+var repo = github.getRepo("tumblenet", "beta.tumblenet.cu.cc");
 
-    return repo.createCommit("HEAD", author, committer, "message", oid, [parent]);
-  })
-  .done(function(commitId) {
-    console.log("New Commit: ", commitId);
-    rimraf(path.resolve(__dirname, "tmp"), function () { 
-      console.log('done');
-    });
-
-  });
+var options = {
+  author: {name: 'TumbleNet API', email: 'admin@tumblenet.ga'},
+  committer: {name: 'TumbleNet Admin', email: 'admin@tumblenet.ga'},
+  encode: true // Whether to base64 encode the file. (default: true)
+}
+repo.writeFile('master', '_data/blog.ga', '[]', 'Update Blog feed', options,
+function(err) {
 
 });
