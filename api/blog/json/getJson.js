0733e20c69d22af1e6fc11ed2969ec150a81a39c
Tumble <tumblegamer@gmail.com>
2018-03-10 22:18:59 +0000

    movement

--- api/blog/json/getJson.js
+++ api/blog/json/getJson.js
@@ -1,1 +1,18 @@
-module.exports = require('../../../parses/json.js');
+const request = require('request');
+
+function getJSON(url, callback) {
+  //var url = "https://www.tumblenet.ga/feed/categories.json";
+
+  request({
+    url: url,
+    json: true
+  }, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      //console.log(body) // Print the json response
+      callback(body);
+    }
+  });
+}
+
+module.exports = getJSON;
--- api/blog/xml/getXml.js
+++ api/blog/xml/getXml.js
@@ -1,1 +1,20 @@
-module.exports = require('../../../parses/xml.js');
+const request = require('request');
+const xml2js = require('xml2js');
+
+function getXML(url, callback) {
+  //var url = "http://tumble1999.wixsite.com/tumblegamer/feed.xml";
+
+  request({
+    url: url,
+    headers: {'Content-Type': 'text/xml'}
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      xml2js.parseString(body,function (err, result) { // covert the xml to js
+        //console.log(result); //print out the js var
+        callback(result);
+      });;
+    }
+  });
+}
+
+module.exports = getXML;
