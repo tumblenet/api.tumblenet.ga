f0149b6322bbdf6654b4d2e55f1c7c79f7596dc1
Tumble <tumblegamer@gmail.com>
2018-03-28 10:50:17 +0100

    movement

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('./log.js');
 
 const autoUpdate = require('./autoUpdate.js');
+const app = require('./app.js');
+const setupServer = require('./server.js');
+
+var server = setupServer(app);
 
 
 
