666c259b6bd424184e800ae0a0c4aa067d40a52e
Tumble <tumblegamer@gmail.com>
2018-05-20 14:22:43 +0100

    hi

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const autoUpdate = require('./autoUpdate.js');
+
+var app = require('./app.js');
+var port = process.env.PORT || 3000;
+var server = http.Server(app);
+
+function listenAction() {
+  console.log("Server listening on port " + port);
+}
+
+server.listen(port, listenAction);
+
+autoUpdate.start(10*1000);
