2f4a4562b4f7fb792be7a900a454d3e1569201ad
Tumble <tumblegamer@gmail.com>
2018-06-27 00:31:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const setupServer = require('webserver');
 require('./log.js');
 
-const autoUpdate = require('./autoUpdate.js');
 const app = require('./app.js');
 
 
 var server = setupServer(app);
 
-autoUpdate.start(process.env.INTERVAL||1000);
-
-const tumblenetdiscord = require('tumblenet-discord-node');
