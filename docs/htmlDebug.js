e19c3eea9fbd710a277cd3ad69c3fb540106e20b
Tumble <tumblegamer@gmail.com>
2019-02-26 20:40:59 +0000

    Update htmlDebug.js

--- docs/htmlDebug.js
+++ docs/htmlDebug.js
@@ -4,6 +4,6 @@ for (var i=0, max=all.length; i < max; i++) {
 	var item = all[i];
 	var colorBorder = (all.length-i)*(256/all.length);
 	
-	item.style.position = "relative";
+	//item.style.position = "relative";
 	item.style.border = `dashed hsl(${colorBorder},50%,50%)`;
 }
