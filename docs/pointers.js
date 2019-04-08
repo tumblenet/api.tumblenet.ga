e52ea41bd2415a9d1c8b4bc96a8325244a14d5ee
Tumble <tumblegamer@gmail.com>
2019-04-08 22:53:08 +0100

    Update pointers.js

--- docs/pointers.js
+++ docs/pointers.js
@@ -1,16 +1,42 @@
 function P(value,newValue) {
-		if(P.pointers === undefined){
-			P.pointers = {next:0};
-		}
-		console.log(typeof value + ": " + value);
-		if(value !== undefined){
-			P.pointers[value.id] = newValue || P.pointers[value.id];
-			return P.pointers[value.id];
-		}
-		if(!new.target) {
-			return new P(value);
-		}
-		this.id = P.pointers.next;
-		P.pointers.next++;
-		P(this,value);
+	if(P.pointers === undefined){
+		P.pointers = {next:0};
 	}
+	var type;
+	try {
+		type = value.constructor.name || typeof value;
+	} catch(e) {
+		type = typeof value;
+	}
+	//console.log(type + ": " + value);
+	if(type == "Function") {
+		value = new value();
+		try {
+			type = value.constructor.name || typeof value;
+		} catch(e) {
+			type = typeof value;
+		}
+	}
+	if(value !== undefined && type == "P"){
+		if(newValue !== undefined) {
+			P.pointers[value.id] = newValue;
+		}
+		return P.pointers[value.id];
+	}
+	if(!new.target) {
+		return new P(value);
+	}
+	this.id = P.pointers.next;
+	this.toString = () => {
+		var value = P.pointers[this.id];
+		var type;
+		try {
+		 type = value.constructor.name || typeof value;
+		} catch(e) {
+			type = typeof value;
+		}
+		return "$" + this.id + ": " + type + " " + P.pointers[this.id];
+	}
+	P.pointers.next++;
+	P(this,value);
+}
